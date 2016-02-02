c081fd96bfd6c40ac8db8f77517b803660c8b427
carlcs <seelbach@gmx.de>
2016-02-02 14:50:51 +0100

    Reformatted code

--- checkboxfield/CheckboxFieldPlugin.php
+++ checkboxfield/CheckboxFieldPlugin.php
@@ -3,38 +3,38 @@ namespace Craft;
 
 class CheckboxFieldPlugin extends BasePlugin
 {
-        function getName()
-        {
-                return 'Checkbox Field';
-        }
+    public function getName()
+    {
+        return 'Checkbox Field';
+    }
 
-        function getVersion()
-        {
-                return '1.0';
-        }
+    public function getVersion()
+    {
+        return '1.0';
+    }
 
-        function getSchemaVersion()
-        {
-                return '1.0';
-        }
+    public function getSchemaVersion()
+    {
+        return '1.0';
+    }
 
-        function getDeveloper()
-        {
-                return 'carlcs';
-        }
+    public function getDeveloper()
+    {
+        return 'carlcs';
+    }
 
-        function getDeveloperUrl()
-        {
-                return 'https://github.com/carlcs/craft-checkboxfield';
-        }
+    public function getDeveloperUrl()
+    {
+        return 'https://github.com/carlcs/craft-checkboxfield';
+    }
 
-        function getDocumentationUrl()
-        {
-                return 'https://github.com/carlcs/craft-checkboxfield';
-        }
+    public function getDocumentationUrl()
+    {
+        return 'https://github.com/carlcs/craft-checkboxfield';
+    }
 
-        function getReleaseFeedUrl()
-        {
-                return 'https://github.com/carlcs/craft-checkboxfield/raw/master/releases.json';
-        }
+    public function getReleaseFeedUrl()
+    {
+        return 'https://github.com/carlcs/craft-checkboxfield/raw/master/releases.json';
+    }
 }
--- checkboxfield/fieldtypes/CheckboxFieldFieldType.php
+++ checkboxfield/fieldtypes/CheckboxFieldFieldType.php
@@ -3,62 +3,61 @@ namespace Craft;
 
 class CheckboxFieldFieldType extends BaseFieldType
 {
-        // Public Methods
-        // =========================================================================
+    // Public Methods
+    // =========================================================================
 
-        public function getName()
-        {
-                return Craft::t('Checkbox');
+    public function getName()
+    {
+        return Craft::t('Checkbox');
+    }
+
+    public function defineContentAttribute()
+    {
+        return AttributeType::Bool;
+    }
+
+    public function getSettingsHtml()
+    {
+        return craft()->templates->render('checkboxField/fieldtypes/checkbox/settings', array(
+            'settings' => $this->getSettings(),
+        ));
+    }
+
+    public function getInputHtml($name, $value)
+    {
+        // If this is a new entry, look for a default option
+        if ($this->isFresh()) {
+            $value = $this->getSettings()->default;
         }
 
-        public function defineContentAttribute()
-        {
-                return AttributeType::Bool;
-        }
+        return craft()->templates->render('checkboxField/fieldtypes/checkbox/input', array(
+            'id'    => craft()->templates->formatInputId($name),
+            'name'  => $name,
+            'value' => (bool) $value,
+            'settings' => $this->getSettings(),
+        ));
+    }
 
-        public function getSettingsHtml()
-        {
-                return craft()->templates->render('checkboxField/fieldtypes/checkbox/settings', array(
-                        'settings' => $this->getSettings(),
-                ));
-        }
+    public function prepValueFromPost($value)
+    {
+        return (bool) $value;
+    }
 
-        public function getInputHtml($name, $value)
-        {
-                // If this is a new entry, look for a default option
-                if ($this->isFresh())
-                {
-                        $value = $this->getSettings()->default;
-                }
+    public function prepValue($value)
+    {
+        // It's stored as '0' in the database, but it's returned as false. Change it back to '0'.
+        return $value == false ? '0' : $value;
+    }
 
-                return craft()->templates->render('checkboxField/fieldtypes/checkbox/input', array(
-                        'id'    => craft()->templates->formatInputId($name),
-                        'name'  => $name,
-                        'value' => (bool) $value,
-                        'settings' => $this->getSettings(),
-                ));
-        }
+    // Protected Methods
+    // =========================================================================
 
-        public function prepValueFromPost($value)
-        {
-                return (bool) $value;
-        }
-
-        public function prepValue($value)
-        {
-                // It's stored as '0' in the database, but it's returned as false. Change it back to '0'.
-                return $value == false ? '0' : $value;
-        }
-
-        // Protected Methods
-        // =========================================================================
-
-        protected function defineSettings()
-        {
-                return array(
-                        'heading' => array(AttributeType::Bool, 'default' => true),
-                        'label'   => array(AttributeType::String),
-                        'default' => array(AttributeType::Bool, 'default' => false),
-                );
-        }
+    protected function defineSettings()
+    {
+        return array(
+            'heading' => array(AttributeType::Bool, 'default' => true),
+            'label'   => array(AttributeType::String),
+            'default' => array(AttributeType::Bool, 'default' => false),
+        );
+    }
 }
